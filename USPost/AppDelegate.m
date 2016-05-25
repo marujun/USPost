f17463201103fbe532d9988e42235f63a3f759be
marujun <marujunyy@qq.com>
2016-05-25 16:38:38 +0800

    sync

--- USPost/AppDelegate.m
+++ USPost/AppDelegate.m
@@ -9,7 +9,9 @@
 #import "AppDelegate.h"
 
 @interface AppDelegate ()
-
+{
+    NSWindowController *newWindowController;
+}
 @end
 
 @implementation AppDelegate
@@ -20,6 +22,28 @@
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication {
+    return YES;
+}
+
+- (NSMenu *)applicationDockMenu:(NSApplication *)sender
+{
+    NSMenu *menu = [[NSMenu alloc] initWithTitle:@"menu_open"];
+    [menu addItemWithTitle:@"打开新的窗口" action:@selector(openNewWindow:) keyEquivalent: @"N"];
+    [menu addItem: [NSMenuItem separatorItem]];
+    
+    return menu;
+}
+
+- (void)openNewWindow:(id)sender
+{
+    if (newWindowController) return;
+    
+    NSStoryboard *storyBoard = [NSStoryboard storyboardWithName:@"Main" bundle:[NSBundle mainBundle]];
+    newWindowController = [storyBoard instantiateControllerWithIdentifier:@"MainWindow"];
+    [newWindowController showWindow:self];
 }
 
 @end
